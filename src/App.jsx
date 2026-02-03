836311d17ba755929dec3802955c71f8f5e03173
chinny CircuitNight <jangmaleo@gmail.com>
2026-02-03 19:00:57 +0700

    Fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/users" replace />} />
       <Route path="/users" element={<UserPages />} />
       <Route path="/users/:id" element={<UserDetailPage />} />
     </Routes>
