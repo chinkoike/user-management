d704cfcd66e5520e5dcce6388638a7e8019523d6
chinny CircuitNight <jangmaleo@gmail.com>
2026-02-03 19:17:37 +0700

    Switch to HashRouter to fix refresh 404 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 // src/main.jsx (or index.js)
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Wrap the App component with BrowserRouter */}
     <HashRouter>
       <App />
     </HashRouter>
